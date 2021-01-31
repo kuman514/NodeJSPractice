74666adf2a8d5ea61f0a67f1578dbf0b13365973
kumankoishi.py <7iggif@naver.com>
2021-01-31 11:36:49 +0900

    Understanding the code of main.js

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ let app = http.createServer((request, response) => {
     return response.writeHead(404)
   }
   response.writeHead(200)
+  console.log(__dirname + url)
   response.end(fs.readFileSync(__dirname + url))
+  //response.end('egoing: ' + url)
 })
 app.listen(3000)
