df3d0ae2d66c9186cdd69e7ab811f00535200e82
kumankoishi.py <7iggif@naver.com>
2021-01-31 16:07:49 +0900

    Practicing parsing URL

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
+
 let app = http.createServer((request, response) => {
-  let url = request.url
-  if (request.url === '/') {
+  let _url = request.url
+  let queryData = url.parse(_url, true).query
+  console.log(queryData.id)
+
+  if (_url === '/') {
     url = '/index.html'
   }
-  if (request.url === '/favicon.ico') {
+  if (_url === '/favicon.ico') {
     return response.writeHead(404)
   }
   response.writeHead(200)
-  console.log(__dirname + url)
-  response.end(fs.readFileSync(__dirname + url))
-  //response.end('egoing: ' + url)
+  console.log(__dirname + _url)
+  response.end(queryData.id)
 })
+
 app.listen(3000)
