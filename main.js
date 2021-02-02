572f08207fb8f52078681390db28e8b4cbdfa72b
kumankoishi.py <7iggif@naver.com>
2021-02-02 16:23:25 +0900

    Practicing 404

--- main.js
+++ main.js
@@ -5,36 +5,35 @@ let url = require('url')
 let app = http.createServer((request, response) => {
   let _url = request.url
   let queryData = url.parse(_url, true).query
+  let pathName = url.parse(_url, true).pathname
   let title = queryData.id
 
-  if (_url === '/') {
-    title = 'Welcome'
+  if (pathName === '/') {
+    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+      let template = `
+      <!DOCTYPE html>
+      <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+      </head>
+      <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+      </body>
+      `
+      response.writeHead(200)
+      response.end(template)
+    })
+  } else {
+    response.writeHead(404)
+    response.end('File not found')
   }
-  if (_url === '/favicon.ico') {
-    return response.writeHead(404)
-  }
-
-  response.writeHead(200)
-  fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-    let template = `
-    <!DOCTYPE html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-    </body>
-    `
-    response.end(template)
-  })
 })
 
 app.listen(3000)
