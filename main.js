2782d7ddbf213ea4590016102a3cc33ae4f02903
kumankoishi.py <7iggif@naver.com>
2021-02-05 20:32:07 +0900

    Making update page

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ let app = http.createServer((request, response) => {
     title = 'Create'
     queryData.id = 'Create'
     let template = templateHTML(list, title, `
-      <form action="http://localhost:3000/create_process" method="post">
+      <form action="/create_process" method="post">
         <p>
           <input type="text" name="title" placeholder="Title">
         </p>
@@ -89,8 +89,28 @@ let app = http.createServer((request, response) => {
       })
     })
   } else if (pathName === '/update') {
-    response.writeHead(200)
-    response.end('update page')
+    let list = '<ul></ul>'
+    fs.readdir('./data', (err, filelist) => {
+      list = templateList(filelist)
+    })
+    fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
+      let template = templateHTML(list, title, `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p>
+            <input type="text" name="title" placeholder="Title" value="${title}">
+          </p>
+          <p>
+            <textarea name="description" rows="8" cols="200" placeholder="Description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `)
+      response.writeHead(200)
+      response.end(template)
+    })
   } else {
     response.writeHead(404)
     response.end('File not found')
